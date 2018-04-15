f2949d7cb8de25b9e2a10f258270072cd1eae623
mayur_d_developer <dmayur57@gmail.com>
2018-04-15 15:09:36 +0530

    added Superagent.

--- routes/mail.js
+++ routes/mail.js
@@ -4,6 +4,8 @@ var router = express.Router();
 var authHelper = require('../helpers/auth');
 var graph = require('@microsoft/microsoft-graph-client');
 
+const request = require('superagent');
+
 /* GET /mail */
 router.get('/', async function(req, res, next) {
   let parms = { title: 'Inbox', active: { inbox: true } };
@@ -13,38 +15,60 @@ router.get('/', async function(req, res, next) {
 
   if (accessToken && userName) {
     parms.user = userName;
-
-    // Initialize Graph client
-    const client = graph.Client.init({
-      debugLogging: true,
-      authProvider: function (did) {
-        did(null, accessToken);
-      }
-    });
-    console.log(client);
+    var getMailsURL = encodeURI('https://graph.microsoft.com/v1.0/me/mailfolders/inbox/messages?$select=subject,from,receivedDateTime,isRead&$filter=isRead eq false&$orderby=receivedDateTime DESC');
+    console.log(getMailsURL);
     
-
-    try {
-      // Get the 10 newest messages from inbox
-      const result = await client
-      .api('/me/mailfolders/inbox/messages?$select=subject,from,receivedDateTime,isRead&$filter=isRead eq false&$orderby=receivedDateTime DESC')
-      .top(5)
-      // .select('subject,from,receivedDateTime,isRead')
-      // .filter('isRead eq false') //
-      // .orderby('receivedDateTime DESC')
-      .get();
-
-      // console.log(result);
-      
-
-      parms.messages = result.value;
-      res.render('mail', parms);
-    } catch (err) {
+    request
+   .get(getMailsURL)
+   .set('Authorization', 'Bearer ' + accessToken)
+   .end((err, result) => {
+    //  callback(err, res);
+    if(err){
       parms.message = 'Error retrieving messages';
       parms.error = { status: `${err.code}: ${err.message}` };
       parms.debug = JSON.stringify(err.body, null, 2);
       res.render('error', parms);
+    }else{
+        parms.messages = JSON.parse(result.text).value;
+        // console.log(result);
+        // console.log(parms.messages);
+        // res.send(parms.messages);
+        res.render('mail', parms);
     }
+   });
+
+
+    // Initialize Graph client
+    // const client = graph.Client.init({
+    //   debugLogging: true,
+    //   authProvider: function (did) {
+    //     did(null, accessToken);
+    //   }
+    // });
+    // console.log(client);
+    
+
+    // try {
+    //   // Get the 10 newest messages from inbox
+    //   const result = await client
+    //   .api('/me/mailfolders/inbox/messages?$select=subject,from,receivedDateTime,isRead&$filter=isRead eq false&$orderby=receivedDateTime DESC')
+    //   .top(5)
+    //   // .select('subject,from,receivedDateTime,isRead')
+    //   // .filter('isRead eq false') //
+    //   // .orderby('receivedDateTime DESC')
+    //   .get();
+
+    //   // console.log(result);
+      
+
+    //   parms.messages = result.value;
+    //   res.render('mail', parms);
+    // } catch (err) {
+    //   parms.message = 'Error retrieving messages';
+    //   parms.error = { status: `${err.code}: ${err.message}` };
+    //   parms.debug = JSON.stringify(err.body, null, 2);
+    //   res.render('error', parms);
+    // }
     
   } else {
     // Redirect to home
