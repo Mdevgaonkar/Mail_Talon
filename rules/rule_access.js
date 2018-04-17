1f2490c16cbfaa1fb605b5648cccdabd86681a48
mayur_d_developer <dmayur57@gmail.com>
2018-04-17 10:34:57 +0530

    added geting all rules from rule list

--- rules/rule_access.js
+++ rules/rule_access.js
@@ -1,12 +1,26 @@
 const fs = require ('fs');
 
+var ruleList;
+var rules;
+
 fs.readFile(__dirname + '/ruleList.json','utf8',function(err,data){
     if(err){
         // console.log(err);
     }else{
+        //get all rules in a list
         // console.log(JSON.parse(data));
     }
 });
+
+function getAllRules(){
+    //based on rule list array
+    // get all rules in an array 
+    ruleList.forEach(rule => {
+        //add rule to rules array
+    });
+}
+
+
 
 
 // fs.readFile('file', 'utf8', function (err, data) {
