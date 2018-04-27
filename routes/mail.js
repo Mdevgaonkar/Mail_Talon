665161b27240438455b0dd9db0aab1d6609b6179
mayur_d_developer <dmayur57@gmail.com>
2018-04-28 02:40:00 +0530

    added request with proxy and header

--- routes/mail.js
+++ routes/mail.js
@@ -1,14 +1,17 @@
-
 var express = require('express');
 var router = express.Router();
 var authHelper = require('../helpers/auth');
-var graph = require('@microsoft/microsoft-graph-client');
 
-const request = require('superagent');
+const request = require('request');
 
 /* GET /mail */
-router.get('/', async function(req, res, next) {
-  let parms = { title: 'Inbox', active: { inbox: true } };
+router.get('/', async function (req, res, next) {
+  let parms = {
+    title: 'Inbox',
+    active: {
+      inbox: true
+    }
+  };
 
   const accessToken = await authHelper.getAccessToken(req.cookies, res);
   const userName = req.cookies.graph_user_name;
@@ -16,60 +19,36 @@ router.get('/', async function(req, res, next) {
   if (accessToken && userName) {
     parms.user = userName;
     var getMailsURL = encodeURI('https://graph.microsoft.com/v1.0/me/mailfolders/inbox/messages?$select=subject,from,receivedDateTime,isRead&$filter=isRead eq false&$orderby=receivedDateTime DESC');
-    console.log(getMailsURL);
-    
+    // console.log(getMailsURL);
+
     request
-   .get(getMailsURL)
-   .set('Authorization', 'Bearer ' + accessToken)
-   .end((err, result) => {
-    //  callback(err, res);
-    if(err){
-      parms.message = 'Error retrieving messages';
-      parms.error = { status: `${err.code}: ${err.message}` };
-      parms.debug = JSON.stringify(err.body, null, 2);
-      res.render('error', parms);
-    }else{
-        parms.messages = JSON.parse(result.text).value;
-        // console.log(result);
-        // console.log(parms.messages);
-        // res.send(parms.messages);
-        res.render('mail', parms);
-    }
-   });
+      .get({
+        uri: getMailsURL, // proxy:'http://proxy.server.com', 
+        headers: {
+          'Authorization': 'Bearer ' + accessToken
+        }
+      }, (err, results, body) => {
+        //err-> if error occurs then will have some prop
+        //result-> provides raw result along with request
+        //body-> provide actual required info
+         console.log('statusCode:', results && results.statusCode);
+        //  console.log('Body:', body);
+        if (err) {
+          parms.message = 'Error retrieving messages';
+          parms.error = {
+            status: `${err.code}: ${err.message}`
+          };
+          parms.debug = JSON.stringify(err.body, null, 2);
+          res.render('error', parms);
+        } else {
+          parms.messages = JSON.parse(body).value;
+          // console.log(result);
+          // console.log(parms.messages);
+          // res.send(parms.messages);
+          res.render('mail', parms);
+        }
+      });
 
-
-    // Initialize Graph client
-    // const client = graph.Client.init({
-    //   debugLogging: true,
-    //   authProvider: function (did) {
-    //     did(null, accessToken);
-    //   }
-    // });
-    // console.log(client);
-    
-
-    // try {
-    //   // Get the 10 newest messages from inbox
-    //   const result = await client
-    //   .api('/me/mailfolders/inbox/messages?$select=subject,from,receivedDateTime,isRead&$filter=isRead eq false&$orderby=receivedDateTime DESC')
-    //   .top(5)
-    //   // .select('subject,from,receivedDateTime,isRead')
-    //   // .filter('isRead eq false') //
-    //   // .orderby('receivedDateTime DESC')
-    //   .get();
-
-    //   // console.log(result);
-      
-
-    //   parms.messages = result.value;
-    //   res.render('mail', parms);
-    // } catch (err) {
-    //   parms.message = 'Error retrieving messages';
-    //   parms.error = { status: `${err.code}: ${err.message}` };
-    //   parms.debug = JSON.stringify(err.body, null, 2);
-    //   res.render('error', parms);
-    // }
-    
   } else {
     // Redirect to home
     res.redirect('/');
