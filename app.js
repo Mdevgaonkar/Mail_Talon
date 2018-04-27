70f96de1ea9f141637bcc14932d2a7d3fa67ac0e
mayur_d_developer <dmayur57@gmail.com>
2018-04-28 02:34:00 +0530

    added refresh route

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/authorize', authorize);
 app.use('/mail', mail);
-app.use('/getNewMails', run_comp);
+app.use('/refresh', run_comp);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
