101019d029a11bf14abbfb248c37bde0b57ac925
mayur_d_developer <dmayur57@gmail.com>
2018-05-15 17:26:00 +0530

    removing render commands

--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,25 @@ var authHelper = require('../helpers/auth');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  let parms = { title: 'Home', active: { home: true } };
+  let parms = {};
 
+  console.log(req.cookies);
+  
   const accessToken = await authHelper.getAccessToken(req.cookies, res);
   const userName = req.cookies.graph_user_name;
 
   if (accessToken && userName) {
     parms.user = userName;
-    parms.debug = `User: ${userName}\nAccess Token: ${accessToken}`;
+    parms.accessToken = accessToken;
+    // parms.debug = `User: ${userName}\nAccess Token: ${accessToken}`;
   } else {
     parms.signInUrl = authHelper.getAuthUrl();
-    parms.debug = parms.signInUrl;
+    // parms.debug = parms.signInUrl;
   }
 
-  res.render('index', parms);
+  res.setHeader('Content-Type', 'application/json');
+  console.log(res.cookies);
+  res.send(parms);
 });
 
 module.exports = router;
