a93441661ed95426e5ec77fa9029347345ac7b46
mayur_d_developer <dmayur57@gmail.com>
2018-05-18 16:40:00 +0530

    removing logs adding getUnreadMails

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var authorize = require('./routes/authorize');
 //for urls like http://localhost:3000/authorize?code=M179b25f8-2481-1aa5-7b73-8263eaebfd61
 // var mail = require('./routes/mail');
 var run_comp = require('./routes/run_comparator');
+var getUnreadMails = require('./routes/getUnreadMails');
 
 
 var app = express();
@@ -20,7 +21,9 @@ var app = express();
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -28,16 +31,17 @@ app.use('/', index);
 app.use('/authorize', authorize);
 // app.use('/mail', mail);
 app.use('/refresh', run_comp);
+app.use('/getUnreadMails', getUnreadMails);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- helpers/auth.js
+++ helpers/auth.js
@@ -1,4 +1,3 @@
-
 const credentials = {
   client: {
     id: process.env.APP_ID,
@@ -12,13 +11,13 @@ const credentials = {
 };
 const oauth2 = require('simple-oauth2').create(credentials);
 const jwt = require('jsonwebtoken');
-    
+
 function getAuthUrl() {
   const returnVal = oauth2.authorizationCode.authorizeURL({
     redirect_uri: process.env.REDIRECT_URI,
     scope: process.env.APP_SCOPES
   });
-  console.log(`Generated auth url: ${returnVal}`);
+  // console.log(`Generated auth url: ${returnVal}`);
   return returnVal;
 }
 
@@ -30,7 +29,7 @@ async function getTokenFromCode(auth_code, res) {
   });
 
   const token = oauth2.accessToken.create(result);
-  console.log('Token created: ', token.token);
+  // console.log('Token created: ', token.token);
 
   saveValuesToCookie(token, res);
 
@@ -56,7 +55,9 @@ async function getAccessToken(cookies, res) {
   // refresh token?
   const refresh_token = cookies.graph_refresh_token;
   if (refresh_token) {
-    const newToken = await oauth2.accessToken.create({refresh_token: refresh_token}).refresh();
+    const newToken = await oauth2.accessToken.create({
+      refresh_token: refresh_token
+    }).refresh();
     saveValuesToCookie(newToken, res);
     return newToken.token.access_token;
   }
@@ -70,21 +71,45 @@ function saveValuesToCookie(token, res) {
   const user = jwt.decode(token.token.id_token);
 
   // Save the access token in a cookie
-  res.cookie('graph_access_token', token.token.access_token, {maxAge: 3600000, httpOnly: true});
+  res.cookie('graph_access_token', token.token.access_token, {
+    maxAge: 3600000,
+    httpOnly: true
+  });
   // Save the user's name in a cookie
-  res.cookie('graph_user_name', user.name, {maxAge: 3600000, httpOnly: true});
+  res.cookie('graph_user_name', user.name, {
+    maxAge: 3600000,
+    httpOnly: true
+  });
   // Save the refresh token in a cookie
-  res.cookie('graph_refresh_token', token.token.refresh_token, {maxAge: 7200000, httpOnly: true});
+  res.cookie('graph_refresh_token', token.token.refresh_token, {
+    maxAge: 7200000,
+    httpOnly: true
+  });
   // Save the token expiration tiem in a cookie
-  res.cookie('graph_token_expires', token.token.expires_at.getTime(), {maxAge: 3600000, httpOnly: true});
+  res.cookie('graph_token_expires', token.token.expires_at.getTime(), {
+    maxAge: 3600000,
+    httpOnly: true
+  });
 }
 
 function clearCookies(res) {
   // Clear cookies
-  res.clearCookie('graph_access_token', {maxAge: 3600000, httpOnly: true});
-  res.clearCookie('graph_user_name', {maxAge: 3600000, httpOnly: true});
-  res.clearCookie('graph_refresh_token', {maxAge: 7200000, httpOnly: true});
-  res.clearCookie('graph_token_expires', {maxAge: 3600000, httpOnly: true});
+  res.clearCookie('graph_access_token', {
+    maxAge: 3600000,
+    httpOnly: true
+  });
+  res.clearCookie('graph_user_name', {
+    maxAge: 3600000,
+    httpOnly: true
+  });
+  res.clearCookie('graph_refresh_token', {
+    maxAge: 7200000,
+    httpOnly: true
+  });
+  res.clearCookie('graph_token_expires', {
+    maxAge: 3600000,
+    httpOnly: true
+  });
 }
 
 exports.getAuthUrl = getAuthUrl;
