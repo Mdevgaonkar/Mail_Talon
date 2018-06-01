dc3caa2eccc20be341dc7d5060cf48fe11a4398f
mayur_d_developer <dmayur57@gmail.com>
2018-06-02 01:59:00 +0530

    updated from_compare function

--- rules/comparers/from_compare.js
+++ rules/comparers/from_compare.js
@@ -1,7 +1,10 @@
-function compare_senders(message_sender, rule_senders) {
-    let mssage_senders = rule_senders;
-    if (mssage_senders.length > 0) {
-        return mssage_senders.indexOf(message_sender);
+function compare_senders(msg_sender, rule_senders) {
+    msg_sender = msg_sender.toLowerCase();
+    let message_senders = rule_senders.map(mail_sender => {
+        return mail_sender.toLowerCase();
+    });
+    if (message_senders.length > 0) {
+        return message_senders.indexOf(msg_sender);
     }
     return -1;
 }
