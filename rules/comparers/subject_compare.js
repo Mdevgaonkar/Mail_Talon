0b631592a77f52af213eb3b642bff8bf274cc489
mayur_d_developer <dmayur57@gmail.com>
2018-06-02 02:38:00 +0530

    updated subject_compare function

--- rules/comparers/subject_compare.js
+++ rules/comparers/subject_compare.js
@@ -1,13 +1,13 @@
 function compare_subject(message_subject, rule_subject) {
-    if (rule_subject.condition === 'equals') {
+    if (rule_subject.condition === "equals") {
         if (message_subject === rule_subject.text) {
             return true;
         }
-    } else if (rule_subject.condition === 'includes') {
+    } else if (rule_subject.condition === "includes") {
         if (message_subject.includes(rule_subject.text)) {
             return true;
         }
-    } else if (rule_subject.condition === 'match') {
+    } else if (rule_subject.condition === "match") {
         var matches = message_subject.match(rule_subject.text);
         if (matches) {
             return true;
