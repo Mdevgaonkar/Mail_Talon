4db724383ea69295b948b7318887cc15c9b5bd96
mayur_d_developer <dmayur57@gmail.com>
2018-06-02 01:19:00 +0530

    updated body_compare function

--- rules/comparers/body_compare.js
+++ rules/comparers/body_compare.js
@@ -1,22 +1,41 @@
+const he = require("he");
+
+function cleanBody(body_string, newLinesAllowed) {
+    const stripedHtml = body_string.replace(/<[^>]+>/g, "");
+    let stripedNewLines = "";
+    if (newLinesAllowed) {
+        stripedNewLines = stripedHtml.replace(/[\n][\r]/g, "");
+    } else {
+        stripedNewLines = stripedHtml.replace(/[\r][\n]/g, "");
+    }
+
+    const decodedStripedHtml = he.decode(stripedNewLines);
+    return decodedStripedHtml;
+}
+
 function compare_body(message_body, rule_body) {
-    if (rule_body.condition === 'equals') {
-        if (message_body === rule_body.text) {
+    msg_body = cleanBody(message_body, false);
+    if (rule_body.condition === "equals") {
+        if (msg_body === rule_body.text) {
             return true;
-        }
-    } else if (rule_body.condition === 'includes') {
-        if (message_body.includes(rule_body.text)) {
+        } else false;
+    } else if (rule_body.condition === "includes") {
+        if (msg_body.includes(rule_body.text)) {
             return true;
+        } else false;
+    } else if (rule_body.condition === "match") {
+        match_exp = new RegExp(rule_body.text, "gi");
+        var matches = match_exp[Symbol.match](msg_body);
+        if (matches == null) {
+            return false;
         }
-    } else if (rule_body.condition === 'match') {
-        var matches = message_body.match(rule_body.text);
-        if (matches) {
-            return true;
-        }
+        return matches.length > 0 ? true : false;
     } else {
         return false;
     }
 }
 
 exports.compare_body = compare_body;
-exports.compare_bodyPreview = compare_body_Preview;
-exports.compare_uniqueBody = compare_uniqueBody;
+exports.cleanBody = cleanBody;
+// exports.compare_bodyPreview = compare_body_Preview;
+// exports.compare_uniqueBody = compare_uniqueBody;
