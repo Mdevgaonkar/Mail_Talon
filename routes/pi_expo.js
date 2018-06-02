421e1d02b53e84957271b1b6dbe9ed5e42a47c43
mayur_d_developer <dmayur57@gmail.com>
2018-06-03 00:38:00 +0530

    Added PI Expo file in route

---
+++ routes/pi_expo.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var router = express.Router();
+var authHelper = require("../helpers/auth");
+const utils = require("../helpers/utils");
+const excel_utils = require("../helpers/excel_helper");
+const pi_logger = require("../rules/PI_export/pi_logger");
+
+const request = require("request");
+const rule_access = require("../rules/rule_access");
+const fs = require("fs");
+
+/* get /piexpo */
+router.get("/", async (req, res, next) => {
+  res.setHeader("Content-Type", "application/json");
+  let parms = {
+    module: "pi_expo",
+    auth: false,
+    errors: [],
+    debug: [],
+    body: {
+      message: ""
+    }
+  };
+
+  const accessToken = await authHelper.getAccessToken(req.cookies, res);
+
+  if (accessToken) {
+    // parms.user = userName;
+    parms.auth = true;
+    parms.body.message = "Starting PI logger process";
+  } else {
+    // Redirect to home
+    res.redirect("/");
+  }
+
+  res.send(parms);
+  setInterval(() => {
+    // pi_logger.log_process(req);
+    excel_utils.log_PI_to_excel(req);
+  }, 1000 * 20 * 1); //1000 * 60 * 2
+});
+
+module.exports = router;
