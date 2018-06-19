812bfeaa80ead5fabe30ced3034c348cb7a4bca7
mayur_d_developer <dmayur57@gmail.com>
2018-06-19 14:28:00 +0530

    updated to become more modular

--- routes/getUnreadMails.js
+++ routes/getUnreadMails.js
@@ -1,18 +1,19 @@
-var express = require('express');
-var router = express.Router();
-var authHelper = require('../helpers/auth');
-
 const request = require('request');
-const rule_access = require('../rules/rule_access');
+const express = require('express');
+const router = express.Router();
+const utils = require('../helpers/utils');
+const authHelper = require('../helpers/auth');
+const savedMailProps = require('../helpers/lastMailProps');
 const fs = require('fs');
-
-
+const lastTimestamp_Fl = __dirname + '/../rules/LastMailTime.json';
 
 /* GET /getUnreadMails */
 router.get('/', async function (req, res, next) {
     let parms = {
         module: 'getUnreadMails',
-        auth: false
+        auth: false,
+        errors: [],
+        debug: []
     };
 
     const accessToken = await authHelper.getAccessToken(req.cookies, res);
@@ -20,8 +21,8 @@ router.get('/', async function (req, res, next) {
     if (accessToken) {
         // parms.user = userName;
         parms.auth = true;
-        var getMailsURL = getUnreadMailsURL();
-        // console.log(getMailsURL);
+        var getMailsURL = getUnreadMailsURL(req.cookies.lastChecked);
+
 
         request
             .get({
@@ -30,88 +31,54 @@ router.get('/', async function (req, res, next) {
                     'Authorization': 'Bearer ' + accessToken
                 }
             }, (err, results, body) => {
-                //err-> if error occurs then will have some prop
-                //result-> provides raw result along with request
-                //body-> provide actual required info
-                console.log('statusCode:', results && results.statusCode);
-                // console.log('Body:', body);
-                if (err) {
-                    parms = sendError(parms, err);
-                } else {
-                    parms.body = formatBody(JSON.parse(body));
-                }
-                res.send(parms);
-            });
+                utils.handleResponse(err, results, body, parms, (parms, body) => {
+                    if (parms.body.indexOf('succeeded')) {
+                        parms.body = formatBody(body);
+                        if (typeof parms.body == 'object' && ('messages' in parms.body) && (parms.body.messages.length > 0)) {
+                            parms.body.lastChecked = parms.body.messages[0].receivedDateTime;
+                            savedMailProps.saveLastMailPropsToCookies(parms.body.messages[0], res);
+                        }
+                    }
+                    res.send(parms);
 
+                });
+            })
     } else {
         // Redirect to home
         res.redirect('/');
     }
 });
 
-module.exports = router;
 
-const getUnreadMailsURL = () => {
-    var lastMailTime;
-    fs.readFile(__dirname + '/../rules/lastmail.json', 'utf8', (err, data) => {
-        if (err) throw err;
-        lastMailTime = data;
-    });
-    if (lastMailTime != null) {
-        return encodeURI('https://graph.microsoft.com/v1.0/me/messages?$count=true&$select=receivedDateTime,subject,isRead,from,ccRecipients,body,bodyPreview,uniqueBody,importance&$filter=isRead eq false and receivedDateTime gt ' + lastMailTime.latestreceivedDateTime + '&$orderby=receivedDateTime DESC');
+
+const getUnreadMailsURL = (latestreceivedDateTime) => {
+
+    if (latestreceivedDateTime == null || latestreceivedDateTime == undefined) {
+        latestreceivedDateTime = fs.readFileSync(lastTimestamp_Fl, 'utf8');
+    }
+
+    if (latestreceivedDateTime != null) {
+        return encodeURI('https://graph.microsoft.com/v1.0/me/messages?$count=true&$select=receivedDateTime,subject,isRead,from,ccRecipients,body,bodyPreview,uniqueBody,importance&$filter=isRead eq false and receivedDateTime gt ' + latestreceivedDateTime + '&$orderby=receivedDateTime DESC');
     } else {
         return encodeURI('https://graph.microsoft.com/v1.0/me/messages?$count=true&$select=receivedDateTime,subject,isRead,from,ccRecipients,body,bodyPreview,uniqueBody,importance&$filter=isRead eq false&$orderby=receivedDateTime DESC');
     }
 }
 
-const Error = (parms, err) => {
-    parms.message = 'Error retrieving messages';
-    parms.error = {
-        status: `${err.code}: ${err.message}`
-    };
-    parms.debug = JSON.stringify(err.body, null, 2);
-    return parms
-
-}
-
 var responseBodyModel = {
     count: 0,
+    lastChecked: '',
     messages: []
-}
-
-const formatMessage = (message) => {
-    var {
-        receivedDateTime,
-        subject,
-        isRead,
-        from,
-        ccRecipients,
-        body,
-        bodyPreview,
-        uniqueBody,
-        importance
-
-    } = message
-
-    return {
-        receivedDateTime: receivedDateTime,
-        subject: subject,
-        isRead: isRead,
-        from: from,
-        ccRecipients: ccRecipients,
-        body: body,
-        bodyPreview: bodyPreview,
-        uniqueBody: uniqueBody,
-        importance: importance
-    }
 }
 
 const formatBody = (body) => {
     const count = body["@odata.count"];
     responseBodyModel.count = count;
     responseBodyModel.messages = body.value.map((message) => {
-        return formatedMessage = formatMessage(message);
-
-    })
+        return formatedMessage = utils.formatMessage(message);
+    });
     return responseBodyModel;
 }
+
+
+
+module.exports = router;
