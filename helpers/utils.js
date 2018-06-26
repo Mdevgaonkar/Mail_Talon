3912f8aa4b142850cde9185ae9c122e8c7c2b61e
Devgaonkar <mayur.devgaonkar@capgemini.com>
2018-06-26 08:28:26 -0400

    improved error handling sequence in handle response

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,84 +1,95 @@
 function formErrorObj(err, err_msg) {
+  error = {
+    message: err_msg,
+    status: `${err.code}: ${err.message}`
+  };
 
-    error = {
-        message: err_msg,
-        status: `${err.code}: ${err.message}`
-    };
-
-    return error;
-
+  return error;
 }
 
 function formatMessage(message) {
-    var {
-        receivedDateTime,
-        subject,
-        isRead,
-        from,
-        ccRecipients,
-        body,
-        bodyPreview,
-        uniqueBody,
-        importance
+  var {
+    receivedDateTime,
+    subject,
+    isRead,
+    from,
+    ccRecipients,
+    body,
+    bodyPreview,
+    uniqueBody,
+    importance
+  } = message;
 
-    } = message
-
-    return {
-        receivedDateTime: receivedDateTime,
-        subject: subject,
-        isRead: isRead,
-        from: from,
-        ccRecipients: ccRecipients,
-        body: body,
-        bodyPreview: bodyPreview,
-        uniqueBody: uniqueBody,
-        importance: importance
-    }
+  return {
+    receivedDateTime: receivedDateTime,
+    subject: subject,
+    isRead: isRead,
+    from: from,
+    ccRecipients: ccRecipients,
+    body: body,
+    bodyPreview: bodyPreview,
+    uniqueBody: uniqueBody,
+    importance: importance
+  };
 }
-
 
 const handleResponse = (err, results, body, parms, done) => {
-    //err-> if error occurs then will have some prop
-    //result-> provides raw result along with request
-    //body-> provide actual required info
+  //err-> if error occurs then will have some prop
+  //result-> provides raw result along with request
+  //body-> provide actual required info
+
+  // console.log('statusCode:', results && results.statusCode);
+  // console.log(parms);
+
+  if (err) {
     try {
-        err = JSON.parse(err);
-        body = JSON.parse(body);
+      err = JSON.parse(err);
     } catch (e) {
-        console.log(e); // error in the above string (in this case, yes)!
-        parms.body = 'Request Failed';
-        parms.errors.push(formErrorObj(e, 'Error retrieving response'));
-        parms.debug.push({
-            detail: `${JSON.stringify(e.body)}`
-        });
-        return done(parms, body);
+      console.log('Error: '+err);
+      console.log(e); // error in the above string (in this case, yes)!
+      parms.body = "Request Failed";
+      parms.errors.push(formErrorObj(e, "Error retrieving response"));
+      parms.debug.push({
+        detail: `${JSON.stringify(e.body)}`
+      });
+      return done(parms, body);
     }
 
-    // console.log('statusCode:', results && results.statusCode);
-    // console.log(parms);
-
-    if (err) {
-        parms.body = 'Request Failed';
-        parms.errors.push(formErrorObj(err, 'Error retrieving response'));
-        parms.debug.push({
-            detail: `${JSON.stringify(err.body)}`
-        });
-        return done(parms, body);
-    } else if ('error' in body) {
-        parms.body = 'Request Failed';
-        parms.errors.push(formErrorObj(body.error, 'Error retrieving messages'));
-        parms.debug.push({
-            detail: body.error.innerError
-        });
-        return done(parms, body);
+    parms.body = "Request Failed";
+    parms.errors.push(formErrorObj(err, "Error retrieving response"));
+    parms.debug.push({
+      detail: `${JSON.stringify(err.body)}`
+    });
+    return done(parms, body);
+  } else {
+    try {
+      body = JSON.parse(body);
+    } catch (e) {
+      console.log(body);
+      console.log(e); // error in the above string (in this case, yes)!
+      parms.body = "Request Failed";
+      parms.errors.push(formErrorObj(e, "Error retrieving response"));
+      parms.debug.push({
+        detail: `${JSON.stringify(e.body)}`
+      });
+      return done(parms, body);
+    }
+    if ("error" in body) {
+      parms.body = "Request Failed";
+      parms.errors.push(formErrorObj(body.error, "Error retrieving messages"));
+      parms.debug.push({
+        detail: body.error.innerError
+      });
+      return done(parms, body);
     } else {
-        parms.body = 'Request Succeeded';
-        parms.debug.push({
-            detail: 'Nothing to debug ...yet'
-        });
-        return done(parms, body);
+      parms.body = "Request Succeeded";
+      parms.debug.push({
+        detail: "Nothing to debug ...yet"
+      });
+      return done(parms, body);
     }
-}
+  }
+};
 
 exports.error = formErrorObj;
 exports.formatMessage = formatMessage;
