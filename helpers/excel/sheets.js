0904365f5386ae17ec61b7188113f32d65f2fcb7
mayur_d_developer <dmayur57@gmail.com>
2018-07-02 01:59:05 +0530

    added creat rows methods

--- helpers/excel/sheets.js
+++ helpers/excel/sheets.js
@@ -10,8 +10,7 @@ function getAllSheets(accessToken, excel_drive_item_id) {
   };
 
   return new Promise(resolve => {
-    request.get(
-      {
+    request.get({
         uri: `https://graph.microsoft.com/v1.0/me/drive/items/${excel_drive_item_id}/workbook/worksheets`,
         proxy: process.env.proxyURL,
         headers: {
@@ -23,14 +22,14 @@ function getAllSheets(accessToken, excel_drive_item_id) {
           if (parms.body.indexOf("succeeded")) {
             parms.body = {};
             let sheets = body.value;
-            if(sheets != undefined || sheets != null){
+            if (sheets != undefined || sheets != null) {
               parms.body.sheets = sheets.map(sheet => {
                 return sheet.name;
               });
-            }else{
+            } else {
               parms.errors.push(utils.error(body, "could not fetch sheet list"));
             }
-            
+
           }
           resolve(parms);
         });
@@ -47,8 +46,7 @@ function createNewSheet(accessToken, excel_drive_item_id, sheetName) {
   };
 
   return new Promise(resolve => {
-    request.post(
-      {
+    request.post({
         uri: `https://graph.microsoft.com/v1.0/me/drive/items/${excel_drive_item_id}/workbook/worksheets/add`,
         proxy: process.env.proxyURL,
         headers: {
@@ -90,8 +88,7 @@ function createNewTable(
   };
 
   return new Promise(resolve => {
-    request.post(
-      {
+    request.post({
         uri: `https://graph.microsoft.com/v1.0/me/drive/items/${excel_drive_item_id}/workbook/tables/add`,
         proxy: process.env.proxyURL,
         headers: {
@@ -104,15 +101,15 @@ function createNewTable(
         }
       },
       (err, results, body) => {
-        utils.handleResponse(err, results, body, parms,async (parms, body) => {
+        utils.handleResponse(err, results, body, parms, async (parms, body) => {
           if (parms.body.indexOf("succeeded")) {
 
-            let rename_table = await renameTable(accessToken,excel_drive_item_id,body.name, tableName);
-            if(rename_table.body){
+            let rename_table = await renameTable(accessToken, excel_drive_item_id, body.name, tableName);
+            if (rename_table.body) {
               parms.body = true;
-            }else{
+            } else {
               parms.body = false;
-            parms.errors.push(utils.error(body, "could not rename new table"));
+              parms.errors.push(utils.error(body, "could not rename new table"));
             }
           } else {
             parms.body = false;
@@ -138,8 +135,7 @@ function renameTable(
   };
 
   return new Promise(resolve => {
-    request.patch(
-      {
+    request.patch({
         uri: `https://graph.microsoft.com/v1.0/me/drive/items/${excel_drive_item_id}/workbook/tables/${oldTableIdName}`,
         proxy: process.env.proxyURL,
         headers: {
@@ -157,6 +153,42 @@ function renameTable(
           } else {
             parms.body = false;
             parms.errors.push(utils.error(body, "could not rename table"));
+          }
+          resolve(parms);
+        });
+      }
+    );
+  });
+}
+
+function createRows(accessToken, excel_drive_item_id, sheetName) {
+  let parms = {
+    module: "create_new_sheet",
+    errors: [],
+    debug: []
+  };
+
+  return new Promise(resolve => {
+    request.post({
+        uri: `https://graph.microsoft.com/v1.0/me/drive/items/${excel_drive_item_id}/workbook/worksheets/add`,
+        proxy: process.env.proxyURL,
+        headers: {
+          Authorization: "Bearer " + accessToken,
+          "Content-Type": "application/json"
+        },
+        json: {
+          name: sheetName
+        }
+      },
+      (err, results, body) => {
+        utils.handleResponse(err, results, body, parms, (parms, body) => {
+          if (parms.body.indexOf("succeeded")) {
+            if (sheetName === body.name) {
+              parms.body = true;
+            }
+          } else {
+            parms.body = false;
+            parms.errors.push(utils.error(body, "could not create new sheet"));
           }
           resolve(parms);
         });
