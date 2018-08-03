f7a4db524fbbfdad5e67674fb097dce3e7366bf9
Mdevgaonkar <dmayur57@gmail.com>
2018-08-02 22:00:00 -0400

    started making compare loop

--- routes/run_comparator.js
+++ routes/run_comparator.js
@@ -1,22 +1,39 @@
 var express = require('express');
 var router = express.Router();
 var authHelper = require('../helpers/auth');
+var getMails = require('../helpers/mail/getMails');
+var getMailsUrl = require('../helpers/url_helper/urls');
 
 const request = require('request');
 const rule_access = require('../rules/rule_access');
 const fs = require('fs');
 
 /* post /compare */
-router.post('/', (req, res, next) => {
-  console.log(req.headers["content-type"]);
+router.get('/', async (req, res, next) => {
+  res.setHeader('Content-Type', 'application/json');
+  let parms = {
+    module: 'run_comparator',
+    auth: false,
+    errors: [],
+    debug: []
+  };
 
-  if (req.headers["content-type"] === 'application/json') {
-    res.send(req.body);
-  } else {
+  const accessToken = await authHelper.getAccessToken(req.cookies, res);
+  if (accessToken) {
+      parms.auth = true;
+      const recent10Mails = await getMails(accessToken,getMailsUrl());
+      console.log(recent10Mails.body.length);
+      if(typeof recent10Mails.body==='object' && recent10Mails.body.length>0){
+        rule_access.applyRules()
+      }
+      
 
+
+
+  }else{
+    // Redirect to home
+    res.redirect('/');
   }
-
-
 });
 
 module.exports = router;
