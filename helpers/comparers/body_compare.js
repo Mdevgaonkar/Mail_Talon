280914f7bfd76936191253c46ffcd103815d3f98
Mdevgaonkar <dmayur57@gmail.com>
2018-08-08 22:00:00 -0400

    added  body comparer

---
+++ helpers/comparers/body_compare.js
@@ -0,0 +1,41 @@
+const he = require("he");
+
+function cleanBody(body_string, newLinesAllowed) {
+    const stripedHtml = body_string.replace(/<[^>]+>/g, "");
+    let stripedNewLines = "";
+    if (newLinesAllowed) {
+        stripedNewLines = stripedHtml.replace(/[\n][\r]/g, "");
+    } else {
+        stripedNewLines = stripedHtml.replace(/[\r][\n]/g, "");
+    }
+
+    const decodedStripedHtml = he.decode(stripedNewLines);
+    return decodedStripedHtml;
+}
+
+function compare_body(message_body, rule_body) {
+    msg_body = cleanBody(message_body, false);
+    if (rule_body.condition === "equals") {
+        if (msg_body === rule_body.text) {
+            return true;
+        } else false;
+    } else if (rule_body.condition === "includes") {
+        if (msg_body.includes(rule_body.text)) {
+            return true;
+        } else false;
+    } else if (rule_body.condition === "match") {
+        match_exp = new RegExp(rule_body.text, "gi");
+        var matches = match_exp[Symbol.match](msg_body);
+        if (matches == null) {
+            return false;
+        }
+        return matches.length > 0 ? true : false;
+    } else {
+        return false;
+    }
+}
+
+exports.compare = compare_body;
+exports.cleanBody = cleanBody;
+exports.compare_bodyPreview = compare_body;
+exports.compare_uniqueBody = compare_body;
