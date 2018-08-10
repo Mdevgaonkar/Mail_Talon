857f538e7be1abdff2bb55c1fd051b84625da828
Mdevgaonkar <dmayur57@gmail.com>
2018-08-09 22:00:00 -0400

    added from comparer

---
+++ helpers/comparers/from_compare.js
@@ -0,0 +1,12 @@
+function compare_senders(msg_sender, rule_senders) {
+    msg_sender = msg_sender.toLowerCase();
+    let message_senders = rule_senders.map(mail_sender => {
+        return mail_sender.toLowerCase();
+    });
+    if (message_senders.length > 0) {
+        return message_senders.indexOf(msg_sender);
+    }
+    return -1;
+}
+
+exports.compare = compare_senders;
