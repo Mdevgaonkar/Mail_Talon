17d3884d49b33292ed1428c5e4a9427a63b9a725
Mdevgaonkar <dmayur57@gmail.com>
2018-08-19 22:13:00 +0530

    added get recent mails urls

--- helpers/url_helper/urls.js
+++ helpers/url_helper/urls.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
 const last_fetch_time_fl = __dirname + '/last_fetch_time';
+const lastTimestamp_Fl = __dirname + "/../LastMailTime.json";
 
 const getUnreadMailsURL = (latestreceivedDateTime) => {
 
@@ -18,9 +20,15 @@ const getUnreadMailsURL = (latestreceivedDateTime) => {
     last_fetch_time = fs.readFileSync(last_fetch_time_fl, 'utf8');
     
   
-    if (latestreceivedDateTime != null) {
-        return encodeURI('https://graph.microsoft.com/v1.0/me/messages?$count=true&$select=receivedDateTime,subject,isRead,from,ccRecipients,body,bodyPreview,uniqueBody,importance&$filter=isRead eq false and receivedDateTime gt ' + latestreceivedDateTime + '&$orderby=receivedDateTime');
+    if (last_fetch_time != null) {
+        return encodeURI('https://graph.microsoft.com/v1.0/me/messages?$count=true&$select=receivedDateTime,subject,isRead,from,ccRecipients,body,bodyPreview,uniqueBody,importance&$filter=receivedDateTime gt ' + last_fetch_time + '&$orderby=receivedDateTime');
     } else {
-        return encodeURI('https://graph.microsoft.com/v1.0/me/messages?$count=true&$select=receivedDateTime,subject,isRead,from,ccRecipients,body,bodyPreview,uniqueBody,importance&$filter=isRead eq false&$orderby=receivedDateTime');
+        d = new Date();
+        last_fetch_time = d.toISOString(d.setHours(d.getHours() - 1 ));
+        return encodeURI('https://graph.microsoft.com/v1.0/me/messages?$count=true&$select=receivedDateTime,subject,isRead,from,ccRecipients,body,bodyPreview,uniqueBody,importance&$filter=receivedDateTime gt '+last_fetch_time+'&$orderby=receivedDateTime');
     }
   }
+
+
+  exports.getUnreadMailsURL = getUnreadMailsURL;
+  exports.getRecentMails = getRecentMails;
