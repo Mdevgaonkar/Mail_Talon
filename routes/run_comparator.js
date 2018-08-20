1549185891799cb11d42bfb53a8c4f900ae354ed
Mdevgaonkar <dmayur57@gmail.com>
2018-08-20 22:13:00 +0530

    getting started wit the loop for rules

--- routes/run_comparator.js
+++ routes/run_comparator.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var authHelper = require('../helpers/auth');
 var getMails = require('../helpers/mail/getMails');
-var getMailsUrl = require('../helpers/url_helper/urls');
+var url_helper = require('../helpers/url_helper/urls');
 
 const request = require('request');
 const rule_access = require('../rules/rule_access');
@@ -21,10 +21,14 @@ router.get('/', async (req, res, next) => {
   const accessToken = await authHelper.getAccessToken(req.cookies, res);
   if (accessToken) {
       parms.auth = true;
-      const recent10Mails = await getMails(accessToken,getMailsUrl());
-      console.log(recent10Mails.body.length);
-      if(typeof recent10Mails.body==='object' && recent10Mails.body.length>0){
-        rule_access.applyRules()
+      console.log(url_helper.getRecentMails());
+      
+      const recent10Mails = await getMails(accessToken,url_helper.getRecentMails());
+      if(typeof recent10Mails.body==='object'&& (recent10Mails.body.messages instanceof Array) && recent10Mails.body.count>0){
+        console.log(recent10Mails.body.messages.length);
+        
+        console.log(rule_access.applyRules(recent10Mails.body.messages));
+        
       }
       
 
