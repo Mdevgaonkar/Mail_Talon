7c2c7edc2cbc0c969722591804648ab26ebbd00b
Mdevgaonkar <dmayur57@gmail.com>
2018-08-24 22:13:00 +0530

    Added action helper for excel loging action

---
+++ helpers/actions/logToExcel.js
@@ -0,0 +1,85 @@
+const request = require("request");
+const authHelper = require("../auth");
+const sheets = require("../excel/sheets");
+const utils = require("../utils");
+
+
+async function log_to_excel(req, rows, excel_drive_item_path,current_sheet_name,current_table_name, num_of_cols) {
+  console.log("excel logging started");
+
+  let parms = {
+    module: "excel_helper",
+    auth: false,
+    errors: [],
+    debug: []
+  };
+
+
+  const accessToken = await authHelper.getAccessToken(req.cookies);
+  if (accessToken) {
+    parms.auth = true;
+    //get all sheet names
+    let sheet_list = await sheets.getAllSheetsFromWorkbook(
+      accessToken,
+      excel_drive_item_path
+    );
+
+
+    let address = sheets.columnsToAddress(num_of_cols);
+
+
+    if (sheet_list.body.sheets.indexOf(current_sheet_name) == -1) {
+      //create new sheet
+      console.log("making new sheet");
+
+      let new_sheet = await sheets.createNewSheet(
+        accessToken,
+        excel_drive_item_path,
+        current_sheet_name
+      );
+      if ('body' in new_sheet) {
+        //create new table
+        console.log("making new table");
+
+        let new_table = await sheets.createNewTable(
+          accessToken,
+          excel_drive_item_path,
+          current_sheet_name,
+          current_table_name,
+          address
+        );
+        if ('body' in  new_table) {
+          console.log("new table created");
+        } else {
+          console.log(new_table);
+          parms.errors.push(
+            utils.error(new_table.errors, "create new table failed")
+          );
+          parms.debug.push({
+            detail: `${JSON.stringify(new_table)}`
+          });
+        }
+      } else {
+        console.error(new_sheet);
+        parms.errors.push(
+          utils.error(new_sheet.errors, "create new Sheet failed")
+        );
+        parms.debug.push({
+          detail: `${JSON.stringify(new_sheet)}`
+        });
+        parms.errors.push(new_sheet);
+      }
+    }
+
+    //write to that sheet
+    console.log("writing to table");
+    return await sheets.createRows(
+      accessToken,
+      excel_drive_item_path,
+      current_table_name,
+      rows
+    );
+  }
+}
+
+exports.log_PI_to_excel = log_PI_to_excel;
