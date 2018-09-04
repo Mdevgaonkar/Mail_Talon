3898395b18c4e439f9d93dc678346e8663bc04d3
Mdevgaonkar <dmayur57@gmail.com>
2018-09-04 13:01:00 +0530

    Changed parameters of log_to_excel to take from action keys

--- helpers/actions/logToExcel.js
+++ helpers/actions/logToExcel.js
@@ -4,7 +4,7 @@ const sheets = require("../excel/sheets");
 const utils = require("../utils");
 
 
-async function log_to_excel(req, rows, excel_drive_item_path,current_sheet_name,current_table_name, num_of_cols) {
+async function log_to_excel(req, rows, action) {
   console.log("excel logging started");
 
   let parms = {
@@ -13,6 +13,12 @@ async function log_to_excel(req, rows, excel_drive_item_path,current_sheet_name,
     errors: [],
     debug: []
   };
+
+  var actionProps = getActionProps(action);
+  excel_drive_item_path=actionProps.excel_drive_item_path;
+  current_sheet_name=actionProps.current_sheet_name;
+  current_table_name=actionProps.current_table_name;
+  num_of_cols=actionProps.num_of_cols;
 
 
   const accessToken = await authHelper.getAccessToken(req.cookies);
@@ -80,6 +86,35 @@ async function log_to_excel(req, rows, excel_drive_item_path,current_sheet_name,
       rows
     );
   }
+}
+
+
+function getActionProps (action){
+  // action = {
+  //   "type": "excel_add_row",
+  //   "excel_path": "/PI_online.xlsx",
+  //   "sheet_name": {
+  //     "eval": true,
+  //     "name": "new Date().toLocaleString('en-us', {month: 'short'})+'_'+new Date().toLocaleString('en-us', {year: '2-digit'})"
+  //   },
+  //   "table_name": {
+  //     "eval": true,
+  //     "name": "new Date().toLocaleString('en-us', {month: 'short'})+'_'+new Date().toLocaleString('en-us', {year: '2-digit'})"
+  //   },
+  //   "num_of_cols":12
+  // }
+
+  
+  current_sheet_name = action.sheet_name.eval? eval(action.sheet_name.name) : action.sheet_name.name;
+  current_table_name = action.table_name.eval? eval(action.table_name.name) : action.table_name.name;
+
+  return {
+    excel_drive_item_path:action.excel_path,
+    current_sheet_name:current_sheet_name,
+    current_table_name:current_table_name,
+    num_of_cols:action.num_of_cols
+  }
+
 }
 
 exports.log_PI_to_excel = log_PI_to_excel;
